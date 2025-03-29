8107bba499369f68c1f8e9d33bf4a4a552f93a03
shellipal <palshelli5@gmail.com>
2025-03-29 21:11:12 +0530

    Reverse

--- 02_Array/02_arr.cpp
+++ 02_Array/02_arr.cpp
@@ -12,11 +12,45 @@ int LinearSearch(int arr[], int size, int target)
     }
     return -1;
 }
+
+// int main()
+// {
+//     int arr[5] = {2, 5, 8, 77, 54};
+//     int size = 5;
+//     int target = 5;
+
+//     cout << LinearSearch(arr, size, target) << endl;
+// }
+
+void ReverseArray(int arr[], int size)
+{
+    int start = 0;
+    int end = size - 1;
+
+    while (start < end)
+    {
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+}
+
 int main()
 {
-    int arr[5] = {2, 5, 8, 77, 54};
-    int size = 5;
-    int target = 5;
+    int arr[] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
+    int size = 10;
+
+    // int arr[5] = {2, 5, 8, 77, 54};
+    // int size = 5;
+    int target = 12;
 
     cout << LinearSearch(arr, size, target) << endl;
+    ReverseArray(arr, size);
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
 }
