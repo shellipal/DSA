c840cc4e34e697de8050f70e3e9b8d86043bf23c
shellipal <palshelli5@gmail.com>
2025-03-31 23:52:50 +0530

    vec function

--- 02_Array/03_vector.cpp
+++ 02_Array/03_vector.cpp
@@ -5,17 +5,33 @@ using namespace std;
 int main()
 {
     // vector<int> vec = {1, 2, 3};
-    vector<int> vec(5, 0);
+    // vector<int> vec(5, 0);
     // cout << vec[0] << endl;
     // cout << vec[1] << endl;
     // cout << vec[2] << endl;
     // cout << vec[3] << endl;
     // cout << vec[4] << endl;
 
-    for (int i : vec)
-    {
-        cout << i << endl;
-    }
+    vector<int> vec;
 
+    cout << "size : " << vec.size() << endl;
+    vec.push_back(22);
+    vec.push_back(33);
+    vec.push_back(44);
+
+    cout << "After push back size : " << vec.size() << endl;
+
+    vec.pop_back();
+
+    cout << "After pop back size : " << vec.size() << endl;
+
+    // for (int i : vec) // for each loop.........
+    // {
+    //     cout << i << endl;
+    // }
+
+    // cout << vec.front() << endl;
+    // cout << vec.back() << endl;
+    cout << vec.at(0) << endl;
     return 0;
 }
