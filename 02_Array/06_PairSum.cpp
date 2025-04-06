a1b58d2921522177a7a341d85aafa21afbb0838a
shellipal <palshelli5@gmail.com>
2025-04-06 23:07:35 +0530

    2 pointer approch

--- 02_Array/06_PairSum.cpp
+++ 02_Array/06_PairSum.cpp
@@ -7,25 +7,47 @@ vector<int> pairSum(vector<int> nums, int target)
     vector<int> ans;
 
     int n = nums.size();
-    for (int i = 0; i < n; i++)
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = i + 1; j < n; j++)
+    //     {
+    //         if (nums[i] + nums[j] == target)
+    //         {
+    //             ans.push_back(i);
+    //             ans.push_back(j);
+    //             return ans;
+    //         }
+    //     }
+    // }
+
+    int i = 0, j = n - 1;
+
+    while (i < j)
     {
-        for (int j = i + 1; j < n; j++)
+        int pairSum = nums[i] + nums[j];
+        if (pairSum > target)
         {
-            if (nums[i] + nums[j] == target)
-            {
-                ans.push_back(i);
-                ans.push_back(j);
-                return ans;
-            }
+            j--;
+        }
+        else if (pairSum < target)
+        {
+            i++;
+        }
+        else
+        {
+            ans.push_back(i);
+            ans.push_back(j);
+            return ans;
         }
     }
+
     return ans;
 }
 
 int main()
 {
     vector<int> nums = {2, 7, 11, 15};
-    int target = 13;
+    int target = 26;
 
     vector<int> ans = pairSum(nums, target);
     cout << ans[0] << ", " << ans[1] << endl;
