6b6925489c1fb21f0e5648bbb8aff7e846adf683
shellipal <palshelli5@gmail.com>
2025-06-04 12:51:18 +0530

    string

---
+++ 07_Strings/02_String.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    // string str = "Shelli pal";
+    // string str1 = "hello";
+    // string str2 = "everyone";
+
+    // string str3 = str1 + " " + str2;
+    // cout << str3 << endl;
+    // cout << (str1 == str2) << endl;
+    // cout << (str1 < str2) << endl;
+
+    string str;
+
+    getline(cin, str);
+
+    cout << str << endl;
+
+    // str = "hello EveryOne";
+    // cout << str << endl;
+}
