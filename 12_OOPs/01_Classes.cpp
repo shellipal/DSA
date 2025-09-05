df6b1cbd0bbeae8d2372b65b4a282e2af5afc5de
shellipal <palshelli5@gmail.com>
2025-09-05 22:36:24 +0530

    OOPs Part-1

---
+++ 12_OOPs/01_Classes.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Teacher
+{
+
+private:
+    double salary;
+
+public:
+    // properties / attributes...
+    string name;
+    string dept;
+    string subject;
+
+    // methods...
+    void changeDept(string newdept)
+    {
+        dept = newdept;
+    }
+
+    // constructor.....
+    // non-perameterized....
+    Teacher()
+    {
+        dept = "Computer Science";
+    }
+    // perameterized...
+    Teacher(string name, string dept, string subject, double salary)
+    {
+        this->name = name;
+        this->dept = dept;
+        this->subject = subject;
+        this->salary = salary;
+    }
+
+    void getInfo()
+    {
+        cout << "Name: " << name << endl;
+        cout << "Subject: " << subject << endl;
+        cout << "Department: " << dept << endl;
+        cout << "Salary: " << salary << endl;
+    }
+
+    /*
+        // setter...
+        void setSalary(double s)
+        {
+            salary = s;
+        }
+        // getter...
+        double getSalary()
+        {
+            return salary;
+        }
+    */
+    /*
+    class Account
+    {
+    private:
+        double balance;
+        int password; // data Hiding...
+
+    public:
+        string username;
+        int accountID;
+    };
+    */
+};
+
+int main()
+{
+    Teacher t1("shelli", "computerScience", "c++", 25000);
+
+    // t1.name = "Shelli";
+    // // t1.dept = "CS";
+    // t1.subject = "C++";
+    // t1.setSalary(25000);
+
+    // cout << t1.name << endl;
+    // cout << t1.getSalary() << endl;
+    // cout << t1.dept << endl;
+
+    t1.getInfo();
+    return 0;
+}
