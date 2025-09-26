4fc6b347e1cdf7c3882112ed4a72bf55a2bd3b4d
shellipal <palshelli5@gmail.com>
2025-09-26 20:27:04 +0530

    Stack using LL

---
+++ 14_Stack/02_basics2.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+#include<List>
+using namespace std;
+
+class Stack{
+    list<int>ll;
+
+    public:
+    void push(int val)
+    {
+        ll.push_front(val);
+    }
+
+    void pop(){
+        ll.pop_front();
+    }
+
+    int top(){
+        return ll.front();
+    }
+
+    bool empty(){
+       return ll.size() == 0;
+    }
+};
+
+int main()
+{
+ Stack s;
+
+ s.push(10);
+ s.push(20);
+ s.push(30);
+
+  while(!s.empty())
+ {
+    cout<<s.top() <<"->";
+    s.pop();
+ }
+    cout<<"NULL"<<endl;
+    return 0;
+}
