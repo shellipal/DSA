4bb7176d3dda8fe0d267d409ffddf02c0727c26d
shellipal <palshelli5@gmail.com>
2025-10-07 20:14:30 +0530

    trapWater_optimal

---
+++ 14_Stack/10_TrapWaterOptimal.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+int trapWater(vector<int> &height)
+{
+    int n = height.size();
+    int ans = 0;
+    int l = 0;
+    int r = n - 1;
+    int lmax = 0, rmax = 0;
+
+    while (l < r)
+    {
+        lmax = max(lmax, height[l]);
+        rmax = max(rmax, height[r]);
+
+        if (lmax < rmax)
+        {
+            ans += lmax - height[l];
+            l++;
+        }
+        else
+        {
+            ans += rmax - height[r];
+            r--;
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    vector<int> height = {4, 2, 0, 3, 2, 5};
+    cout << trapWater(height) << endl;
+    return 0;
+}
