1b6a3195f38154800a35d64a8f98aed15a25c47e
shellipal <palshelli5@gmail.com>
2025-10-13 18:42:33 +0530

    Implement Queue using stack

---
+++ 15_Queue/05_Imple_Q_using_S.cpp
@@ -0,0 +1,52 @@
+// Implememt queue using stack.............
+#include <iostream>
+#include <stack>
+#include <queue>
+using namespace std;
+
+class MyQueue
+{
+public:
+    stack<int> s1;
+    stack<int> s2;
+
+    MyQueue()
+    {
+    }
+
+    void push(int data)
+    {
+        while (!s1.empty())
+        {
+            s2.push(s1.top());
+            s1.pop();
+        }
+        s1.push(data);
+        while (!s2.empty())
+        {
+            s1.push(s2.top());
+            s2.pop();
+        }
+    }
+
+    int pop()
+    {
+        int ans = s1.top();
+        s1.pop();
+        return ans;
+    }
+
+    int peek()
+    {
+        return s1.top();
+    }
+
+    bool empty()
+    {
+        return s1.empty();
+    }
+};
+
+int main()
+{
+}
