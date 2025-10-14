2ef479b085c30ac00d2f081ad02c2e81c3dd7b56
shellipal <palshelli5@gmail.com>
2025-10-14 22:30:11 +0530

    First_Unique_Char problem

---
+++ 15_Queue/06_FirstUniqueChar.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <queue>
+#include <unordered_map>
+using namespace std;
+
+int FirstUniqueChar(string s)
+{
+    unordered_map<char, int> m;
+    queue<int> q;
+
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (m.find(s[i]) == m.end())
+        {
+            q.push(i);
+        }
+        m[s[i]]++;
+
+        while (q.size() > 0 && m[s[q.front()]] > 1)
+        {
+            q.pop();
+        }
+    }
+
+    return q.empty() ? -1 : q.front();
+}
+int main(){
+    
+}
