1c567905f9bb2d2df919a863f859b58cc2599f80
shellipal <palshelli5@gmail.com>
2025-10-21 22:45:55 +0530

    preOrder traversal

--- 16_Tree/01_BinaryTree.cpp
+++ 16_Tree/01_BinaryTree.cpp
@@ -33,13 +33,30 @@ Node *BuildTree(vector<int> preorder)
     return root;
 }
 
+// preorder.......
+void preOrder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    cout << root->data << " ";
+    preOrder(root->left);
+    preOrder(root->right);
+}
+
 int main()
 {
     vector<int> preorder = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
 
     Node *root = BuildTree(preorder);
-    cout << root->data << endl;
-    cout << root->left->data << endl;
-    cout << root->right->data << endl;
+
+    // cout << root->data << endl;
+    // cout << root->left->data << endl;
+    // cout << root->right->data << endl;
+
+    preOrder(root);
+    cout << endl;
     return 0;
 }
